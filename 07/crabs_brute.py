4412881b196da3de7de3cedace64b38f6359fefe
Tobias G <theskynet1337@gmail.com>
2021-12-07 18:29:25 +0100

    part 2 solution can only be +-1 mean

--- 07/crabs_brute.py
+++ 07/crabs_brute.py
@@ -1,14 +1,14 @@
-
+import numpy as np
+import statistics
 crabs = []
 with open('input.txt', 'r') as in_file:
     crabs = list(map(int, in_file.read().strip().split(',')))
-
+npcrabs = np.array(crabs)
+mean = round(statistics.mean(crabs))
 best = 999999999999999999999999999
-for i in range(-1, max(crabs)):
+for i in range(mean-1, mean+1):
     dist = 0
-    for crab in crabs:
-        n = abs(crab - i)
-        dist += n * (n + 1) / 2
+    dist = sum(map(lambda n: n*(n+1)/2, abs(npcrabs - i)))
     if dist < best:
         best = dist
 print(best)
