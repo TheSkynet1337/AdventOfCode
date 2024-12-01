f2dc0502c3faeb2f081d834b2b2a33d8c2a6fa64
Tobias G <theskynet1337@gmail.com>
2024-12-01 15:56:43 +0100

    golf it

--- 2024/01.py
+++ 2024/01.py
@@ -1,12 +1,8 @@
 from collections import Counter
 
-input = []
-with open('01.txt', 'r') as f:
-    input = list(map(int, f.read().split()))
-part1_sum = sum([abs(pair[0] - pair[1])
-                 for pair in zip(sorted(input[::2]), sorted(input[1::2]))])
-print('Part 1:', part1_sum)
+input = list(map(int, open('01.txt', 'r').read().split()))
+print('Part 1:', sum([abs(pair[0] - pair[1])
+                      for pair in zip(sorted(input[::2]), sorted(input[1::2]))]))
 right_dict = Counter(input[1::2])
-similiarity = sum([left_num * right_dict[left_num]
-                   for left_num in input[::2]])
-print('Part 2:', similiarity)
+print('Part 2:', sum([left_num * input[1::2].count(left_num)
+                      for left_num in input[::2]]))
